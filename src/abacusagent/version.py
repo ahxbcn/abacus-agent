3000d43ad844ffa83d8094cba6c8e3b4447e3487
root <pxlxingliang>
2025-09-28 16:43:32 +0800

    fix bug in version

--- src/abacusagent/version.py
+++ src/abacusagent/version.py
@@ -17,9 +17,9 @@ def _no_need_subversion():
         if current_file in modified_files:
             return True
         
-        # If codes in the abacustest directory are modified, indicating the main version is not updated
+        # If codes in the abacusagent directory are modified, indicating the main version is not updated
         for i in modified_files:
-            if i.startswith("abacustest/"):
+            if i.startswith("abacusagent/"):
                 return False
         
         return True
