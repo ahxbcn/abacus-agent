d1bddf54c768bfcf9ad2c04b7046a4737fbc8e57
ahxbcn <2092233074@qq.com>
2025-10-14 11:21:56 +0800

    Change EOS fitting to do cell-relax calculation with `fixed_axes` set to `volume`

--- src/abacusagent/modules/eos.py
+++ src/abacusagent/modules/eos.py
@@ -8,7 +8,6 @@ from abacusagent.modules.submodules.eos import abacus_eos as _abacus_eos
 def abacus_eos(
     abacus_inputs_dir: Path,
     stru_scale_number: int = 3,
-    stru_scale_type: Literal['percentage', 'angstrom'] = 'percentage',
     scale_stepsize: float = 0.02
 ):
     """
@@ -17,19 +16,15 @@ def abacus_eos(
     Args:
         abacus_inputs_dir (Path): Path to the ABACUS input files, which contains the INPUT, STRU, KPT, and pseudopotential or orbital files.
         stru_scale_number (int): Number of structures to generate for EOS calculation.
-        stru_scale_type (Literal['percentage', 'angstrom']): Type of scaling for structures.
-        scale_stepsize (float): Step size for scaling.
-            - 'percentage' means percentage of the original cell size. Default is 0.02, which means 2% of the original cell size.
-            - 'angstrom' means absolute angstrom value. The typical stepsize 0.1 angstrom is recommended for most system.
+        scale_stepsize (float): Step size for scaling. Default is 0.02, which means 2% of the original cell size.
 
     Returns:
         Dict[str, Any]: A dictionary containing EOS calculation results:
             - "eos_work_path" (Path): Working directory for the EOS calculation.
-            - "new_abacus_inputs_dir" (Path): ABACUS input files directory containing the lowest energy structure using the fitted EOS.
             - "eos_fig_path" (Path): Path to the EOS fitting plot (energy vs. volume).
             - "E0" (float): Minimum energy (in eV) from the EOS fit.
             - "V0" (float): Equilibrium volume (in Å³) corresponding to E0.
             - "B0" (float): Bulk modulus (in GPa) at equilibrium volume.
             - "B0_deriv" (float): Pressure derivative of the bulk modulus.
     """
-    return _abacus_eos(abacus_inputs_dir, stru_scale_number, stru_scale_type, scale_stepsize)
+    return _abacus_eos(abacus_inputs_dir, stru_scale_number, scale_stepsize)
